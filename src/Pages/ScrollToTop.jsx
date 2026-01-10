68fc08c550c4b90b4b346ee68a95a941376ea4f9
jayakrishnagolla <121039119+gkrishna142@users.noreply.github.com>
2026-01-10 10:37:53 +0530

    Simplify ScrollToTop by removing window check
    Removed conditional check for window.scrollTo in ScrollToTop component.

--- src/Pages/ScrollToTop.jsx
+++ src/Pages/ScrollToTop.jsx
@@ -5,10 +5,7 @@ function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    // Protect for Jest / jsdom
-    if (typeof window !== "undefined" && window.scrollTo) {
-      window.scrollTo(0, 0);
-    }
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
